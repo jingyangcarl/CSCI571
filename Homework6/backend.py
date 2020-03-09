f0d75c9ef18a75aa571b1edfd6044b30ea3246df
Jing Yang <jyang@smtp.ict.usc.edu>
2020-03-09 16:01:33 -0700

    add source ajax, and search query

--- Homework6/backend.py
+++ Homework6/backend.py
@@ -58,11 +58,52 @@ def articles_clean(articles):
     for index in index_remove:
         articles.pop(index)
 
-@app.route('/search/', methods=['GET', 'POST'])
+@app.route('/fetch_source/<category>')
+def fetch_source(category):
+    
+    # init api client
+    newsapi = NewsApiClient(api_key='99da284c213a46ecb176baabc4eb6b7c')
+
+    # get corresponding sources based on category
+    if 'all' not in category:
+        sources = newsapi.get_sources(category=category, language='en', country = 'us')
+    else:
+        sources = newsapi.get_sources(language='en', country = 'us')
+
+    return sources
+
+@app.route('/search', methods=['GET', 'POST'])
 def search():
+
+    # get form
     form = request.form
-    print(form)
-    return form
+    search_input_keyword = form['search_input_keyword']
+    search_input_from = form['search_input_from']
+    search_input_to = form['search_input_to']
+    search_select_category = form['search_select_category']
+    search_select_source = form['search_select_source']
+    
+    # init api client
+    newsapi = NewsApiClient(api_key='99da284c213a46ecb176baabc4eb6b7c')
+
+    # get articles
+    if 'all' in search_select_source:
+        all_articles = newsapi.get_everything(q=search_input_keyword,
+                                        from_param=search_input_from,
+                                        to=search_input_to,
+                                        language='en',
+                                        sort_by='publishedAt',
+                                        page_size=30)
+    else:
+        all_articles = newsapi.get_everything(q=search_input_keyword,
+                                            sources=search_select_source,
+                                            from_param=search_input_from,
+                                            to=search_input_to,
+                                            language='en',
+                                            sort_by='publishedAt',
+                                            page_size=30)
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
