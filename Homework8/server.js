e8b4064b870166011bdaea8f7614dff2e96d43f8
Jing Yang <jingyang.carl.work@gmail.com>
2020-03-31 18:38:57 -0700

    init client

---
+++ Homework8/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/users', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+app.get('/api/customers', (req, res) => {
+  const customers = [
+    {id: 1, firstName: 'John', lastName: 'Doe'},
+    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+  ];
+
+  res.json(customers);
+});
+
+const port = 5000;
+
+app.listen(port, () => `Server running on port ${port}`);
