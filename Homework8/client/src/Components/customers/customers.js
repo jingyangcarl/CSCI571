ad5ea1a7e8d11577a39de67969133f3736d8fe1b
Jing Yang <jingyang.carl.work@gmail.com>
2020-03-31 19:56:39 -0700

    update configuration

--- Homework8/client/src/Components/customers/customers.js
+++ Homework8/client/src/Components/customers/customers.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import './customers.css';
 
 class Customers extends Component {
+    constructor() {
+        super();
+        this.state = {
+            customers: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/api/customers')
+            .then(res => res.json())
+            .then(customers =>this.setState({customers}, () => console.log('fetched', customers)));
+    }
+
     render() {
         return (
             <div>
                 <h2>Customers</h2>
+                <ul>
+                    {this.state.customers.map(customers =>
+                        <li key={customers.id}>{customers.firstName} {customers.lastName}</li>
+                    )}
+                </ul>
             </div>
         );
     };
