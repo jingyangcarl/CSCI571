084221b353e2d23df3837f4c9e7e39ff7ba5a43d
Jing Yang <jingyang.carl.work@gmail.com>
2020-04-21 18:34:29 -0700

    for deploy

--- Homework8/server.js
+++ Homework8/server.js
@@ -14,7 +14,7 @@ app.use("/home", routerHome);
 app.use("/section", routerSection);
 app.use("/keyword", routerKeyword);
 
-const port = 80;
+const port = 5000;
 var listener = app.listen(port, function() {
   console.log("Listening on port " + listener.address().port);
 });
