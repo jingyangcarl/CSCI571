b6dd91ac3a33561a184806d910676e33553c22b7
Jing Yang <jingyang.carl.work@gmail.com>
2020-04-24 12:19:41 -0700

    get the current location and decode to human readable format

--- Homework9/Homework9/HomeViewController.swift
+++ Homework9/Homework9/HomeViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // For use when the app is open & in the background
+        locationManager.requestAlwaysAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            
+            let geocoder = CLGeocoder()
+            // Look up the location and pass it to the completion handler
+            geocoder.reverseGeocodeLocation(location, completionHandler: { (placemarks, error) -> Void in
+                if error == nil {
+                    let readableLocation = placemarks?[0]
+                    print(readableLocation?.locality as Any)
+                }
+            })
+            
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if (status == CLAuthorizationStatus.denied) {
+            
+        }
+    }
 
 }
 
