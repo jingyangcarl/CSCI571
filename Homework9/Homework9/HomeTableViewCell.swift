237e49c194a03917b0edc81b58c58eb2305bdf94
Jing Yang <jingyang.carl.work@gmail.com>
2020-04-25 17:26:51 -0700

    add constrains for lunch screen and add home table view

---
+++ Homework9/Homework9/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  Homework9
+//
+//  Created by Jing Yang on 4/25/20.
+//  Copyright © 2020 Jing Yang. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
