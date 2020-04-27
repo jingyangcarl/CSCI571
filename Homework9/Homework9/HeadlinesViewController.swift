8f93ac995a81c01dcd0af8bc574ab9879a307f98
Jing Yang <jingyang.carl.work@gmail.com>
2020-04-27 00:58:55 -0700

    save

--- Homework9/Homework9/HeadlinesViewController.swift
+++ Homework9/Homework9/HeadlinesViewController.swift
@@ -16,19 +16,18 @@ class HeadlinesViewController: ButtonBarPagerTabStripViewController {
         // change selected bar color
         settings.style.buttonBarBackgroundColor = .white
         settings.style.buttonBarItemBackgroundColor = .white
-        settings.style.selectedBarBackgroundColor = .cyan
+        settings.style.selectedBarBackgroundColor = .systemBlue
         settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
         settings.style.selectedBarHeight = 2.0
         settings.style.buttonBarMinimumLineSpacing = 0
-        settings.style.buttonBarItemTitleColor = .black
         settings.style.buttonBarItemsShouldFillAvailableWidth = true
         settings.style.buttonBarLeftContentInset = 0
         settings.style.buttonBarRightContentInset = 0
 
         changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
             guard changeCurrentIndex == true else {return}
-            oldCell?.label.textColor = .black
-            newCell?.label.textColor = .cyan
+            oldCell?.label.textColor = .gray
+            newCell?.label.textColor = .systemBlue
         }
         
         super.viewDidLoad()
