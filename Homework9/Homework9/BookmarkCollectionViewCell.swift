aaf2ac9c49c1d190d3654b27e362dca6140521cd
Jing Yang <jingyang.carl.work@gmail.com>
2020-05-04 16:14:01 -0700

    Design Bookmark collection view

---
+++ Homework9/Homework9/BookmarkCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BookmarkCollectionViewCell.swift
+//  Homework9
+//
+//  Created by Jing Yang on 5/4/20.
+//  Copyright © 2020 Jing Yang. All rights reserved.
+//
+
+import UIKit
+
+class BookmarkCollectionViewCell: UICollectionViewCell {
+    
+}
